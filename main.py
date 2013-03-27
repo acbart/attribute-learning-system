03509727c2edaaedca9c4e2c222b34f4891eaf23
Austin Cory Bart <acbart@udel.edu>
2013-03-27 13:19:32 -0400

    Some first stabs at an approach. Nothing works because I tried editing without looking at my textbook. Derp.

---
+++ main.py
@@ -0,0 +1,56 @@
+import mdp
+import pprint
+
+class EntityState(object):
+    __slots__ = ["hp", "attack", "defense", "base_hp", "base_attack", "base_defense"]
+
+def calculate_damage(self_attack, other_defense)
+    return attack / defense
+
+def move_damage_attack(self, state):
+    self, other = state
+    self.hp -= calculate_damage(self.attack, self.defense)
+    
+
+class MovesetMDP(mdp.MDP):
+    """A two-dimensional grid MDP, as in [Figure 17.1].  All you have to do is
+    specify the grid as a list of lists of rewards; use None for an obstacle
+    (unreachable state).  Also, you should specify the terminal states.
+    An action is an (x, y) unit vector; e.g. (1, 0) means move east."""
+    def __init__(self, grid, terminals, init=(0, 0), gamma=.9):
+        grid.reverse() ## because we want row 0 on bottom, not on top
+        MDP.__init__(self, init, actlist=orientations,
+                     terminals=terminals, gamma=gamma)
+        update(self, grid=grid, rows=len(grid), cols=len(grid[0]))
+        for x in range(self.cols):
+            for y in range(self.rows):
+                self.reward[x, y] = grid[y][x]
+                if grid[y][x] is not None:
+                    self.states.add((x, y))
+
+    def T(self, state, action):
+        if action == None:
+            return [(0.0, state)]
+        else:
+            return [(0.8, self.go(state, action)),
+                    (0.1, self.go(state, turn_right(action))),
+                    (0.1, self.go(state, turn_left(action)))]
+
+    def go(self, state, direction):
+        "Return the state that results from going in this direction."
+        state1 = vector_add(state, direction)
+        return if_(state1 in self.states, state1, state)
+
+    def to_grid(self, mapping):
+        """Convert a mapping from (x, y) to v into a [[..., v, ...]] grid."""
+        return list(reversed([[mapping.get((x,y), None)
+                               for x in range(self.cols)]
+                              for y in range(self.rows)]))
+
+    def to_arrows(self, policy):
+        chars = {(1, 0):'>', (0, 1):'^', (-1, 0):'<', (0, -1):'v', None: '.'}
+        return self.to_grid(dict([(s, chars[a]) for (s, a) in policy.items()]))
+
+
+solution = mdp.policy_iteration(m)
+pprint.PrettyPrinter(indent=4).pprint(solution)
